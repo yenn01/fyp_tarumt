5df11cc5e56556eb998a68ebef0aaedc7d816f29
yenn01 <yenn.m17@gmail.com>
2022-12-02 17:22:46 +0800

    Initial Commit from customised template

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,59 @@
 <script>
-	export let name;
+    import anime from 'animejs/lib/anime.es.js';
+    import Toast from "./components/Toast.svelte";
+    import {notifications} from './stores/notifications.js'
+    import { fade, fly } from 'svelte/transition';
+    import Footer from './components/Footer.svelte';
+    import Header from './components/Header.svelte';
+    import {Route} from 'tinro'; 
+    import Input from './components/Input.svelte'
+    import RouteTransition from './components/RouteTransition.svelte';
+
+    const handleSubmission = (eventMsg) => {
+        console.log(eventMsg)
+        
+    }
 </script>
 
+<svelte:head>
+    <title>sens</title>
+</svelte:head>
+
+<Header></Header>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <Toast/>
+    
+    <div class="cont_main">
+        <div class="cont_body">
+            <RouteTransition>
+                <Route path="/">
+                    <Input on:evt_submit={handleSubmission}></Input>
+                </Route>
+                <Route path="/scrapping">Scrapping...</Route>
+            </RouteTransition>
+        </div>
+    </div>
+
 </main>
+<Footer></Footer>
 
 <style>
+
+    :root {
+        --bg-color:  rgb(250,250,250)
+    }
+
+    :global(body) {
+        background: var(--bg-color);
+    }
+    
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		padding: 0.2em;
+		max-width: 500px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+        min-width: 500px;
+        min-height: 60vh;
 	}
 
 	@media (min-width: 640px) {
@@ -27,4 +61,21 @@
 			max-width: none;
 		}
 	}
+
+    .cont_main {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+
+    }
+
+    .cont_body {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin-bottom:1em;
+    }
+
+
+
 </style>
